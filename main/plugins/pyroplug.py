7daa29744f7ef0e69c4a89de1139b16d4f393940
botmakin <119750664+botmakin@users.noreply.github.com>
2023-04-15 07:49:06 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -7,7 +7,7 @@ from main.plugins.progress import progress_for_pyrogram
 from main.plugins.helpers import screenshot
 
 from pyrogram import Client, filters
-from pyrogram.errors import ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid, FloodWait
+from pyrogram.errors import ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid
 from ethon.pyfunc import video_metadata
 from telethon import events
 
@@ -18,15 +18,7 @@ def thumbnail(sender):
          return None
       
 async def check(userbot, client, link):
-    msg_id = 0
-    try:
-        msg_id = int(link.split("/")[-1])
-    except ValueError:
-        if '?single' in link:
-            link_ = link.split("?single")[0]
-            msg_id = int(link_.split("/")[-1])
-        else:
-            return False, "**Invalid Link!**"
+    msg_id = int(link.split("/")[-1])
     if 't.me/c/' in link:
         try:
             chat = int('-100' + str(link.split("/")[-2]))
@@ -44,19 +36,10 @@ async def check(userbot, client, link):
         except Exception:
             return False, "Maybe bot is banned from the chat, or your link is invalid!"
             
-async def get_msg(userbot, client, sender, edit_id, msg_link, i, bulk=False):
+async def get_msg(userbot, client, sender, edit_id, msg_link, i):
     edit = ""
     chat = ""
-    msg_id = 0
-    try:
-        msg_id = int(msg_link.split("/")[-1])
-    except ValueError:
-        if '?single' in msg_link:
-            link_ = msg_link.split("?single")[0]
-            msg_id = int(link_.split("/")[-1])
-        else:
-            await client.edit_message_text(sender, edit_id, "**Invalid Link!**")
-            return None
+    msg_id = int(msg_link.split("/")[-1]) + int(i)
     if 't.me/c/' in msg_link:
         chat = int('-100' + str(msg_link.split("/")[-2]))
         file = ""
@@ -67,13 +50,13 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, bulk=False):
                     edit = await client.edit_message_text(sender, edit_id, "Cloning.")
                     await client.send_message(sender, msg.text.markdown)
                     await edit.delete()
-                    return None
+                    return
             if not msg.media:
                 if msg.text:
                     edit = await client.edit_message_text(sender, edit_id, "Cloning.")
                     await client.send_message(sender, msg.text.markdown)
                     await edit.delete()
-                    return None
+                    return
             edit = await client.edit_message_text(sender, edit_id, "Trying to Download.")
             file = await userbot.download_media(
                 msg,
@@ -89,17 +72,14 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, bulk=False):
             caption = str(file)
             if msg.caption is not None:
                 caption = msg.caption
-            if str(file).split(".")[-1] in ['mkv', 'mp4', 'webm', 'mpe4', 'mpeg']:
-                if str(file).split(".")[-1] in ['webm', 'mkv', 'mpe4', 'mpeg']:
+            if str(file).split(".")[-1] in ['mkv', 'mp4', 'webm']:
+                if str(file).split(".")[-1] in ['webm', 'mkv']:
                     path = str(file).split(".")[0] + ".mp4"
                     os.rename(file, path) 
                     file = str(file).split(".")[0] + ".mp4"
                 data = video_metadata(file)
                 duration = data["duration"]
-                try:
-                    thumb_path = await screenshot(file, duration, sender)
-                except Exception:
-                    thumb_path = None
+                thumb_path = await screenshot(file, duration, sender)
                 await client.send_video(
                     chat_id=sender,
                     video=file,
@@ -115,9 +95,11 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, bulk=False):
                         time.time()
                     )
                 )
+                os.remove(file)
             elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
                 await edit.edit("Uploading photo.")
                 await bot.send_file(sender, file, caption=caption)
+                os.remove(file)
             else:
                 thumb_path=thumbnail(sender)
                 await client.send_document(
@@ -133,44 +115,25 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, bulk=False):
                         time.time()
                     )
                 )
-            os.remove(file)
+                os.remove(file)
             await edit.delete()
-            return None
         except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
             await client.edit_message_text(sender, edit_id, "Have you joined the channel?")
-            return None
-        except FloodWait as fw:
-            print(fw)
-            if bulk is True:
-                return int(fw.x) + 5
-            else:
-                await client.edit_message_text(sender, edit_id, f'Try again after {fw.x} seconds due to floodwait from telegram.')
-                return None
+            return 
         except Exception as e:
-            print(e)
             await client.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`')
-            if os.path.isfile(file) == True:
-                os.remove(file)
-            return None
+            os.remove(file)
+            return 
     else:
         edit = await client.edit_message_text(sender, edit_id, "Cloning.")
         chat =  msg_link.split("/")[-2]
         try:
             await client.copy_message(int(sender), chat, msg_id)
-        except FloodWait as fw:
-            print(fw)
-            if bulk is True:
-                return int(fw.x) + 5
-            else:
-                await client.edit_message_text(sender, edit_id, f'Try again after {fw.x} seconds due to floodwait from telegram.')
-                return None
         except Exception as e:
             print(e)
-            await client.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`')
-            return None
+            return await client.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`')
         await edit.delete()
-        return None
- 
+        
 async def get_bulk_msg(userbot, client, sender, msg_link, i):
     x = await client.send_message(sender, "Processing!")
-    await get_msg(userbot, client, sender, x.message_id, msg_link, i, bulk=True) 
+    await get_msg(userbot, client, sender, x.message_id, msg_link, i) 
